823fa8c25d6bb5a5f2fd8b6aed7a8e877c4299fc
Damian Alexandru <damian.alexandru99@gmail.com>
2020-04-07 19:24:12 +0300

    0.0.3 - added user reviews

--- amazon_scrapper.py
+++ amazon_scrapper.py
@@ -1,5 +1,6 @@
 import scrapy
 import json
+from scrapy.crawler import CrawlerProcess
 
 class AmazonSpider(scrapy.Spider):
     name = 'amazon_search'
@@ -20,17 +21,38 @@ class AmazonSpider(scrapy.Spider):
             
             yield{'title':product_title, 'id':product_id}
             self.first_page = False
-            self.data['title'] = product_title[0]
-            self.data['id'] = product_id[0]
+            # self.data['title'] = product_title[0]
+            # self.data['id'] = product_id[0]
             yield response.follow(f'https://www.amazon.com/dp/{product_id[0]}', callback = self.parse)
         else :
             product_review = response.css('#acrPopover').css('::attr(title)').get()
             self.data['total_product_review'] = product_review
+
+            ratings = response.css('.review-rating>span::text').extract()
+            titles = response.css('.review-title>span::text').extract()   
+            authors = response.css('.a-profile-name::text').extract()
+            dates = response.css('.review-date::text').extract() 
+            review_descriptions = response.css('.review-text-content>span::text').extract()
+            for i in range(0, len(ratings)):
+                review = dict({
+                    'title' : titles[i],
+                    'author' : authors[i],
+                    'date' : dates[i],
+                    'rating' : ratings[i],
+                    'content' : review_descriptions[i]}
+                )
+                self.data['reviews'].append(review)
+
             with open('data_amazon.json', 'w') as json_file:
-                json.dump(self.data, json_file)
-            # TODO : crawl reviews and add them to data (a review should have a title, author, rating and description)
+                json.dump(self.data, json_file, indent=4)
+            
             yield{"product_review" : product_review}
 
 # pip install scrapy
-# scrapy runspider -a title=title+of+searched+book amazon_scrapper.py
-# example: scrapy runspider -a title=Altered+Carbon amazon_scrapper.py
+
+
+
+if __name__ == "__main__":
+    process = CrawlerProcess()
+    process.crawl(AmazonSpider, 'Altered+Carbon')
+    process.start()
